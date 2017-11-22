bc03bc51f49726cce951f48e3cbc74c05b69bdba
Twan Goosen <twan@clarin.eu>
2017-11-22 11:55:36 +0100

    Benchmark script: also create directories and start Tomcat

--- benchmark.sh
+++ benchmark.sh
@@ -1,13 +1,33 @@
 #!/bin/sh
+set -e
+
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 DATA_URL="https://gitlab.com/CLARIN-ERIC/docker-vlo-sample-data/raw/master/image/sample-data.tar.gz"
 DATA_DIR="/var/vlo-benchmark-data/"
+SOLR_DATA_DIR="/var/vlo-benchmark-solrdata"
 DEFAULT_ITERATIONS=1
+
+if ! [ -d "$DATA_DIR" ]; then
+	echo "Creating data directory $DATA_DIR"
+	mkdir -p "$DATA_DIR"
+fi
+
+if ! [ -d "$SOLR_DATA_DIR" ]; then
+	echo "Creating Solr data directory $SOLR_DATA_DIR"
+	mkdir -p "$SOLR_DATA_DIR"
+fi
 
 if [ ! -z "$1" ] && [ $1 -gt 0 ]; then
 	N="$1"
 else
 	N="$DEFAULT_ITERATIONS"
+fi
+
+if curl -L http://localhost:8989 > /dev/null 2>&1; then
+	echo "Found Tomcat running at localhost:8989"
+else
+	echo "Starting Solr server"
+	(cd "$DIR" && ./start-solr.sh)
 fi
 
 echo "Running ${N} iterations"
