6211d6f474b5d448045afaf7aca481f7fc91636b
Twan Goosen <twan@clarin.eu>
2019-11-04 16:39:03 +0100

    pass arguments to benchmark script

--- benchmark-docker.sh
+++ benchmark-docker.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -e
 docker build -t vlo-benchmark:latest ./docker
-docker run --name vlo-benchmark --rm -v $(pwd):/benchmark vlo-benchmark:latest
+docker run --name vlo-benchmark --rm -v $(pwd):/benchmark --entrypoint="/bin/bash" vlo-benchmark:latest /benchmark/benchmark.sh $@
+
